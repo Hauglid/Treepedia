255f9e0b9f723561f16f24308489d1277352feb0
Hauglid <karltha@ifi.uio.no>
2016-04-25 00:44:30 +0200

    added gallery and info xml

--- app/src/main/java/com/hauglidtech/treepedia/Home.java
+++ app/src/main/java/com/hauglidtech/treepedia/Home.java
@@ -1,5 +1,6 @@
 package com.hauglidtech.treepedia;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -83,7 +84,8 @@ public class Home extends AppCompatActivity
         if (id == R.id.nav_home) {
             // Handle the camera action
         } else if (id == R.id.nav_search) {
-
+            Intent intent = new Intent(this, Info.class);
+            startActivity(intent);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_manage) {
